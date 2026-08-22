e880803500c1767821347a5d0871bd7788f1cecb
mherald <dev.palma07@gmail.com>
2026-08-21 21:28:25 -0700

    Fix: Saving config and pushing new build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pangasinan-heritage/', // ⚠️ CHANGE THIS TO YOUR REPO NAME
+  base: '/pangasinan-heritage/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
